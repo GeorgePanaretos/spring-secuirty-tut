038159892e068e0ad93d73a235c0484b538d210f
George Panaretos <georgepanaretos.cac@gmail.com>
2025-01-17 16:55:46 +0100

    LDAP Security

--- admin-web/src/main/resources/schema.sql
+++ admin-web/src/main/resources/schema.sql
@@ -13,16 +13,16 @@ CREATE TABLE orders(
     foreign key (customer_id) references customers(customer_id)
 );
 
-CREATE TABLE users (
-    username VARCHAR(50) NOT NULL PRIMARY KEY,
-    password VARCHAR(500) NOT NULL,
-    enabled BOOLEAN NOT NULL
-);
-
-CREATE TABLE authorities (
-    username VARCHAR(50) NOT NULL,
-    authority VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users(username)
-);
-
-CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
+--CREATE TABLE users (
+--    username VARCHAR(50) NOT NULL PRIMARY KEY,
+--    password VARCHAR(500) NOT NULL,
+--    enabled BOOLEAN NOT NULL
+--);
+--
+--CREATE TABLE authorities (
+--    username VARCHAR(50) NOT NULL,
+--    authority VARCHAR(50) NOT NULL,
+--    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users(username)
+--);
+--
+--CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
