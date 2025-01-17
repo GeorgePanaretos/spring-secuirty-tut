a05acef7b009d41324d9db6b48787f079df012c8
George Panaretos <georgepanaretos.cac@gmail.com>
2025-01-17 13:17:24 +0100

    Login & logout security

--- admin-web/src/main/java/com/frankmoley/lil/adminweb/web/HomeController.java
+++ admin-web/src/main/java/com/frankmoley/lil/adminweb/web/HomeController.java
@@ -3,14 +3,18 @@ package com.frankmoley.lil.adminweb.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String getHome(Model model){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(Model model){
+        return "login";
     }
 }
